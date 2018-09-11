0ffa5f76eff2e31f3d7785e55104f194364204e5
LAPTOP-JHLSCDA7\steve <steve@soaconsultingservices.com>
2018-09-10 22:40:13 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FlexLayoutModule, LayoutDirective} from '@angular/flex-layout';
+import {CustomBreakPointsProvider} from './breakpoints/custom.breakpoints';
+import {CustomShowHideDirective} from './directives/custom-show-hide.directive';
+import {AppComponent} from './app.component';
 
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomShowHideDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FlexLayoutModule
   ],
-  providers: [],
+  providers: [
+    CustomBreakPointsProvider,     // Adds breakpoints for 'print' mediaQueries
+    LayoutDirective
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
